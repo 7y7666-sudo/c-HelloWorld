bd7725e604a8167b7b0294cf74d5f5de5385ebed
student <7y7.666@gmail.com>
2026-09-01 12:52:34 +0300

    Hello world

---
+++ HelloWorld_Bakulin_AA-25-07/HelloWorld_Bakulin_AA-25-07.cpp
@@ -0,0 +1,14 @@
+// HelloWorld_Bakulin_AA-25-07.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+//0. Препроцессоры (#)
+//1. Компилятор
+//2. Компоновщик (linker)
